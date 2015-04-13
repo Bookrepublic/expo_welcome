df7e88404333e0cec2ae6551b116d106e6e49028
Matteo Scurati <matteo.scurati@gmail.com>
2015-04-13 15:59:36 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,32 @@
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
+source 'https://rubygems.org'
+
+gem "middleman", "~>3.3.10"
+
+# Frontend
+gem 'slim'
+gem 'middleman-autoprefixer'
+gem 'sass'
+
+source 'https://rails-assets.org' do
+  gem 'rails-assets-normalize-scss'
+end
+
+# Optimization
+gem 'middleman-minify-html'
+
+# Image optimization
+
+gem 'image_optim'
+gem 'jpegoptim'
+gem 'middleman-imageoptim'
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+# Windows does not come with time zone data
+gem "tzinfo-data", platforms: [:mswin, :mingw]
---
+++ config.rb
@@ -0,0 +1,79 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Autoprefixer
+
+activate :autoprefixer do |config|
+  config.browsers = ['last 3 versions', 'Explorer >= 9']
+  config.cascade = true
+  config.inline = true
+end
+
+# Assetes
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+
+# Heroku
+
+set :build_dir, 'tmp'
+
+# Build-specific configuration
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :minify_html
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
