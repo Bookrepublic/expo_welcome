ee0a9a9c9f4329ef02285ea1ff9d3363be80fc1b
Matteo Scurati <matteo.scurati@gmail.com>
2015-04-14 17:25:44 +0200

    Better config

--- config.rb
+++ config.rb
@@ -75,7 +75,8 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-  activate :minify_html
+  activate :minify_html, remove_intertag_spaces: true
+  activate :gzip
   # Use relative URLs
   # activate :relative_assets
 
